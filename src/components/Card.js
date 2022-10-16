4139b1fc728f24f32d831e86bc67f021f1377a85
Ayaka Inaba <ayakayakaaaa@gmail.com>
2022-10-16 23:41:17 +0900

    learning jsx

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,11 @@
-function Card() {
-  return <div></div>;
+import React from "react";
+
+import "./Card.css";
+
+function Card(props) {
+  const classes = "card" + props.className;
+
+  return <div className={classes}>{props.children}</div>;
 }
 
 export default Card;
