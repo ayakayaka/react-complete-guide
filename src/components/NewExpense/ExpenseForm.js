e810ab1b3799344d04e2c0cf2bd7264e7ca09e78
Ayaka Inaba <ayakayakaaaa@gmail.com>
2022-10-22 22:45:45 +0900

    learning event action

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  const titleChangeHandler = () => {
-    console.log("Title Changed");
+  const titleChangeHandler = (event) => {
+    console.log(event.target.value);
   };
   return (
     <form>
