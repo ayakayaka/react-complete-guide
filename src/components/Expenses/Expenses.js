e004762dcc28bc840d34e57da0533bedc5acf201
Ayaka Inaba <ayakayakaaaa@gmail.com>
2022-11-06 22:51:22 +0900

    fixing error

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,7 +12,7 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
-  const filteredExpenses = props.item.filter((expense) => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
@@ -37,39 +37,6 @@ const Expenses = (props) => {
           onChangeFilter={filterChangeHandler}
         />
         {expensesContent}
-        {/* {filteredExpenses.length === 0 && <p>No Expenses Found.</p>}
-        {filteredExpenses.length === 0 ? (
-          <p>No Expenses Found.</p>
-        ) : (
-          filteredExpenses.map((expense) => (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          )) */}
-        {/* )} */}
-        {/* <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        /> */}
       </Card>
     </div>
   );
